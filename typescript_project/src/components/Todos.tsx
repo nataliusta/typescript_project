bef634ed17448f70adf2c9fa54a2c8ee8ba65db4
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-15 12:46:38 +0300

    Add props to Todos

--- typescript_project/src/components/Todos.tsx
+++ typescript_project/src/components/Todos.tsx
@@ -1,4 +1,11 @@
-function Todos() {
+import React from "react";
+
+const Todos: React.FC<{items: string[]}> = (props) => {
+    return (
+        <ul>
+            {props.items.map(item => <li key={item}>{item}</li>)}
+        </ul>
+    );
 
 }
 
