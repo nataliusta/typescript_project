a8b8a1448c11f3edac3b5790bf5b798566fb541e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-19 17:45:09 +0300

    Add useState to App.tsx

--- typescript_project/src/App.tsx
+++ typescript_project/src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './components/models/todo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos();
+  };
 
   return (
     <div>
