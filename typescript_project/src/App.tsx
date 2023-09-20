4f9d0f02c85dc4086bad019e778371c732d97c5e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-20 18:56:21 +0300

    Add todos-context

--- typescript_project/src/App.tsx
+++ typescript_project/src/App.tsx
@@ -5,21 +5,6 @@ import Todos from './components/Todos';
 import Todo from './components/models/todo';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== todoId);
-    });
-  };
 
   return (
     <div>
--- typescript_project/src/components/Todos.tsx
+++ typescript_project/src/components/Todos.tsx
@@ -4,14 +4,14 @@ import Todo from "./models/todo";
 import TodoItem from "./TodoItem";
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{items: Todo[]; onRemoveTodo: () => void}> = (props) => {
+const Todos: React.FC<{items: Todo[]; onRemoveTodo: (id: string) => void}> = (props) => {
     return (
         <ul className={classes.todos}>
             {props.items.map((item) => (
             <TodoItem 
                 key={item.id} 
                 text={item.text} 
-                onRemoveTodo={props.onRemoveTodo.bind()} />
+                onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />
             ))}
         </ul>
     );
